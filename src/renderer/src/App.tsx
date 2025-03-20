912654dbc54ee09ddfbc417b8290cc7120f5b6e9
HycJack <772403255@qq.com>
2025-03-20 16:55:03 +0800

    add react router

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,58 +1,13 @@
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger
-} from '@/components/ui/alert-dialog'
-import { Button } from '@/components/ui/button'
-import { useState } from 'react'
-// import {ipcRenderer} from 'electron'
+import { Outlet } from "react-router-dom"
+
 
 function App(): JSX.Element {
-  const [filePath, setFilePath] = useState('')
-  const handleClick = (action: string): void => {
-    window.electron.ipcRenderer.invoke('ping').then((msg) => {
-      setFilePath(msg);
-    });
-    
-    window.electron.ipcRenderer.send('set-title', "你好")
-  }
-
-  const handleOpen = (): void => {
-    // 返回值为 void，不能直接赋值给 setFilePath
-    // 假设使用异步方式获取路径并更新状态
-    window.electron.ipcRenderer.invoke('dialog:openFile').then((path) => {
-      setFilePath(path);
-    });
-  }
+  
   return (
     <div className="h-screen w-full flex items-center justify-center">
-      <Button type="button" id="btn" onClick={handleOpen}>Open a File</Button>
-      File path: <strong id="filePath">{filePath}</strong>
-      <Button variant="outline" onClick={()=>handleClick("ping")}>Ping</Button>
-      <AlertDialog>
-        <AlertDialogTrigger asChild>
-          <Button variant="outline">Show Dialog</Button>
-        </AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-            <AlertDialogDescription>
-              This action cannot be undone. This will permanently delete your account and remove
-              your data from our servers.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction>Continue</AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
+      
+      {/* 添加路由内容出口 */}
+      <Outlet />
     </div>
   )
 }
--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,11 +1,20 @@
-import './assets/main.css'
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
+import { Home } from './pages/Home'
+import { About } from './pages/About'
+import './assets/main.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
